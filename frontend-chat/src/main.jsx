bc8c96a16d3f7579659bb6cf901f086560324481
Shivam Gupta <74005091+coderS176@users.noreply.github.com>
2024-12-31 22:44:38 +0530

    added router-dom

--- frontend-chat/src/main.jsx
+++ frontend-chat/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import AppRoutes from './config/routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </StrictMode>,
 )
